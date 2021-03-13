5514d78b90d6dfe653ce2d809232bea9dc1bd9b2
Aayush Manandhar <amanandhar86@gmail.com>
2021-03-13 19:50:46 +0545

    Updated login and registration page

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,13 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input'}))
+    password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput(attrs={'class': 'input'}))
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'input'}),
+            'email': forms.EmailInput(attrs={'class': 'input', 'required': True})
+        }
